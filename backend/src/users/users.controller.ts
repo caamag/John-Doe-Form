8d3db192a82c80790915e102726caf55191a3d67
Caio Lopes <caio.lopes@bcrcx.com>
2026-08-15 12:40:22 -0300

    Adicionando rota de post

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -10,5 +11,8 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
-  
+  @Post()
+  create(@Body() data: CreateUserDto) {
+    return this.usersService.create(data);
+  }
 }
